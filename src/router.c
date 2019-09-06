11e7172373c1a2d6e04b57365c7fe0357b550e7c
Nicolas Kolling Ribas <nicolaskribas@gmail.com>
2019-09-06 11:50:46 -0300

    :poop: Reading the routers config done.

--- src/router.c
+++ src/router.c
@@ -1,22 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-int main(int argc, char *argv[]){
+typedef struct{
+    int id;
+    int port;
+    char ip[15];
+}routerSetings;
+
+void *receiver (){
+
+}
+void *sender(){
+
+}
+
+void main(int argc, char *argv[]){
 
     if(argc != 2){
         printf("[ERROR] %d arguments provided, expectating 1.\n", argc-1);
         exit(0);
     }
 
-    int id = atoi(argv[1]);
+    int i, id, port, n_routers, self_id = *argv[1];
+    char ip[15];
+    routerSetings *routers;
+
 
     FILE *roteador = fopen("../config/roteador.config", "r");
+        for(i = 0, n_routers = 0; fscanf(roteador,"%d %d %s", &id, &port, ip) != EOF; i++)
+        {
+            n_routers++;
+            routers = (routerSetings *) realloc (routers, sizeof(routerSetings) * n_routers);
 
+            routers[i].id = id;
+            routers[i].port = port;
+            sprintf(routers[i].ip, "%s", ip);
+        }
     fclose(roteador);
 
-    FILE *enlaces = fopen("../config/enlaces.config", "r");
+    // int a, b, cost;
+    // FILE *enlaces = fopen("../config/enlaces.config", "r");
+    //     for(i = 0, n_edges = 0; fscanf(elaces,"%d %d %d", %a, %b, %cost) != EOF; i++)
+    //     {
+    //
+    //
+    //     }
+    // fclose(enlaces);
 
-    fclose(enlaces);
-
-    return 0;
+    // pthread_t sender, receiver;
+    // pthread_create(&sender,NULL, sender, NULL);
+    // pthread_create(&receiver,NULL, receiver, NULL);
 }
